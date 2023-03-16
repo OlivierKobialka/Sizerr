918e48c1392a1ccbbab087d5e243f2e1d636d809
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-03-16 11:56:35 +0100

    deleting

--- server/index.js
+++ server/index.js
@@ -7,12 +7,6 @@ const sql = require("mssql");
 
 let db = require("../server/database/Shoes.sql");
 
-// const dbConfig = {
-// 	user: "sa",
-// 	password: "Olivier123!",
-// 	server: "localhost",
-// 	database: "DB-Project",
-// };
 const dbConfig = new sql.ConnectionPool({
 	user: "sa",
 	password: "Olivier123!",
@@ -25,45 +19,42 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
-//? ENDPOINT FOR SHOE MEASUREMENTS
-// app.post("/form-data-S-M", (req, res) => {
-// 	const formData = req.body;
-// 	console.log(formData);
-// 	//? Process the form data and send a response
-// 	const gender = req.query.gender;
-// 	db.query(`SELECT * FROM test WHERE gender = @gender`, (err, result) => {
-// 		if (err) {
-// 			console.error(err);
-// 		} else {
-// 			console.log(result);
-// 		}
-// 	});
-
-// 	res.send(`Gender received: ${gender}`);
-// });
-// describe("Shoe Measurement", () => {
-// 	it("200 Response", () => {
-// app.get("/form-data-S-M", async (req, res) => {
-// 	const gender = req.query.gender;
-// 	try {
-// 		await dbConfig.connect();
-// 		const result = await dbConfig
-// 			.request()
-// 			.query(`SELECT * FROM test WHERE gender = @gender`, { gender });
-// 		const users = result.recordset;
-// 		res.json(users);
-// 	} catch (error) {
-// 		console.log(error);
-// 		res.sendStatus(500);
-// 	} finally {
-// 		dbConfig.close();
-// 	}
-// });
-// 	});
-// });
-
 app.get("/api/Shoes", function (req, res) {
-	getShoeMeasurement();
+	let dbCon = new sql.ConnectionPool(dbConfig);
+	dbCon
+		.connect()
+		.then(function () {
+			const gender = req.query.gender;
+			const unit = req.query.unit;
+			const size = req.query.size;
+			if (gender === "Male") {
+				let request = new sql.Request(dbCon);
+				request.query(
+					`SELECT Brand, Size FROM shoesMen where units = @unit and size = @size`,
+					{ unit, size }
+				);
+			} else {
+				let request = new sql.Request(dbCon);
+				request.query(
+					`SELECT Brand, Size FROM shoesWomen where units = @unit and size = @size`,
+					{ unit, size }
+				);
+			}
+			request
+				.then(function (recordSet) {
+					console.log(recordSet);
+					dbCon.close();
+				})
+				.catch(function (err) {
+					console.log(err);
+					dbCon.close();
+				});
+		})
+		.catch(function (err) {
+			console.log(err);
+			dbCon.close();
+		});
+	//
 	res.send(JSON.stringify({ status: 200, error: null, response: "Success" }));
 });
 
