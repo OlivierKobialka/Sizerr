b946a4ff9e43d8e434b0d49fe68b781a80e37635
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-04-04 19:02:53 +0200

    fixing main files

--- server/index.js
+++ server/index.js
@@ -1,81 +1,119 @@
+const sql = require("mssql");
 const bodyParser = require("body-parser");
 const express = require("express");
-let cors = require("cors");
 const app = express();
+const cors = require("cors");
 const port = 8080;
-const sql = require("mssql");
 
-const shoesMeasurements = require("./routes/Shoes/Shoes-M");
-const shoesBrand = require("./routes/Shoes/Shoes-B");
-const userComment = require("./routes/Analitycs/ReviewsFromUsers");
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.json());
 
-let db = require("../server/database/Shoes.sql");
-
-export const dbConfig = new sql.ConnectionPool({
+const dbConfig = {
 	user: "sa",
 	password: "Olivier123!",
 	server: "localhost",
 	database: "Sizes",
 	options: {
 		encrypt: true,
+		trustServerCertificate: true,
 	},
+};
+const pool = new sql.ConnectionPool(dbConfig);
+
+sql.connect(dbConfig, error => {
+	if (error) {
+		console.log(error);
+	} else {
+		console.log("  Connected to database!");
+	}
 });
 
-//? set middleware
-app.use(dbConfig);
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(cors());
-//? set middleware
+//TODO zrobic kazde app.get || app.post w osobnym pliku jako funkcja
+
+//TODO GO TO => shoes.tsx line 173
+app.get("/api/Shoes-B", async (req, res) => {
+	const { brand, size, gender, measurement } = req.query;
+
+	try {
+		const pool = await sql.connect(dbConfig);
+		const paramSize = `size${measurement.toUpperCase()}`;
+		let result;
+		if (gender === "male") {
+			result = await pool
+				.request()
+				.input("size", size)
+				.query(`SELECT * from shoesMan WHERE ${paramSize}=@size`);
+
+		} else {
+			result = await pool
+				.request()
+				.query(`SELECT * from shoesWoman WHERE ${paramSize} = @size`, [
+					{ name: "size", value: size },
+				]);
+		}
+
+		res.status(200).json({ shoesBrand: result.recordset });
+	} catch (error) {
+		res.status(500).send(`Error: ${error}`);
+	}
+});
+
+app.get("/api/Shoes-M", async (req, res) => {
+	const { unit, size, gender } = req.query;
+
+	try {
+		const pool = await sql.connect(dbConfig);
+
+		let result;
+
+		if (gender === "male" && unit === "cm") {
+			result = await pool.request()
+				.query`SELECT * FROM dbo.shoesMan WHERE sizeCM = CAST(${size} AS NUMERIC(10, 2))`;
+		} else if (gender === "male" && unit === "inch") {
+			result = await pool.request()
+				.query`SELECT * FROM dbo.shoesMan WHERE sizeIN = ${size}`;
+		} else {
+			res.status(400).send("Invalid query parameters");
+			return;
+		}
+
+		res.send(result.recordset);
+	} catch (error) {
+		console.error(error);
+		res.status(500).send("Error");
+	}
+});
 
 app.post("/api/Opinion", async function userComment(req, res) {
-	const { email, comment, category } = req.body;
+	const { email, comment, category } = req.query;
 	try {
-		// await sql.connect(dbConfig);
+		await sql.connect(dbConfig);
 
 		const result =
 			await dbConfig.query`INSERT INTO Comment (email, comment, category) VALUES (${email}, ${comment}, ${category})`;
-
-		res.status(200).json({ message: "Comment added successfully" });
+		res.send("Comment added!");
 	} catch (error) {
 		console.log(error);
-		res.status(500).send("Server Error");
 	} finally {
 		sql.close();
 	}
 });
 
-app.get("/api/opinionCategory", async (req, res) => {
-	try {
-		const result = await dbConfig.query(
-			"SELECT COUNT(*) AS submitCounter FROM Comment"
-		);
-
-		res.status(200).json({ submitCounter: result[0].submitCounter });
-	} catch (error) {
-		console.error(error);
-		res.status(500).json({ message: "Internal server error" });
-	}
-});
-
-// app.post("/api/Opinion", userComment);
-
-app.get("/api/Shoes-M", shoesMeasurements);
-app.get("/api/Shoes-B", shoesBrand);
-
 app.listen(port, () => {
-	function hi() {
-		console.clear();
-		console.log("   ______   ______  ________  ________  _______   _______  ");
-		console.log("  /       /      |/        |/        |/        /         / ");
-		console.log(" /$$$$$$  |$$$$$$/ $$$$$$$$/ $$$$$$$$/ $$$$$$$  |$$$$$$$  |");
-		console.log(" $$ __$$/   $$ |      /$$/  $$ |__    $$ |__$$ |$$ |__$$ | ");
-		console.log(" $$         $$ |     /$$/   $$    |   $$    $$< $$    $$<  ");
-		console.log("  $$$$$$ |  $$ |    /$$/    $$$$$/    $$$$$$$  |$$$$$$$  |");
-		console.log(" /  __$$ | _$$ |_  /$$/____ $$ |_____ $$ |  $$ |$$ |  $$ | ");
-		console.log("$$    $$/ / $$   |/$$      |$$       |$$ |  $$ |$$ |  $$ |");
-		console.log("  $$$$$$/  $$$$$$/ $$$$$$$$/ $$$$$$$$/$$/   $$/ $$/   $$/ ");
-	}
-	hi();
+	// function hi() {
+	// 	console.clear();
+	// 	console.log("   ______   ______  ________  ________  _______   _______  ");
+	// 	console.log("  /       /      |/        |/        |/        /         / ");
+	// 	console.log(" /$$$$$$  |$$$$$$/ $$$$$$$$/ $$$$$$$$/ $$$$$$$  |$$$$$$$  |");
+	// 	console.log(" $$ __$$/   $$ |      /$$/  $$ |__    $$ |__$$ |$$ |__$$ | ");
+	// 	console.log(" $$         $$ |     /$$/   $$    |   $$    $$< $$    $$<  ");
+	// 	console.log("  $$$$$$ |  $$ |    /$$/    $$$$$/    $$$$$$$  |$$$$$$$  |");
+	// 	console.log(" /  __$$ | _$$ |_  /$$/____ $$ |_____ $$ |  $$ |$$ |  $$ | ");
+	// 	console.log("$$    $$/ / $$   |/$$      |$$       |$$ |  $$ |$$ |  $$ |");
+	// 	console.log("  $$$$$$/  $$$$$$/ $$$$$$$$/ $$$$$$$$/$$/   $$/ $$/   $$/ ");
+	// }
+	// hi();
 	console.log(`  Running on http://localhost:${port}`);
 });
