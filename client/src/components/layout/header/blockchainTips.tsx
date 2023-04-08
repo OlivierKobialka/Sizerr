e8961ce9d8c2da4551d27dc1b11d9ad1947dcb3d
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-04-08 18:16:58 +0200

    payable eth button

--- client/src/components/layout/header/blockchainTips.tsx
+++ client/src/components/layout/header/blockchainTips.tsx
@@ -17,17 +17,17 @@ interface TippingDialogProps {
 	onClose: () => void;
 }
 
-// const web3 = new Web3(
-// 	new Web3.providers.HttpProvider(
-// 		"https://mainnet.infura.io/v3/eedf9707ecb447868eaedb02c22fa46c"
-// 	)
-// );
+const web3 = new Web3(
+	new Web3.providers.HttpProvider(
+		"https://mainnet.infura.io/v3/eedf9707ecb447868eaedb02c22fa46c"
+	)
+);
 // const contractAddress = "<CONTRACT_ADDRESS>";
 // // const abi = [<CONTRACT_ABI>];
 
 const BlockchainTips = () => {
 	const [open, setOpen] = useState<boolean>(false);
-	const [amount, setAmount] = useState("");
+	const [account, setAccount] = useState<string>("");
 
 	const handleOpen = () => {
 		setOpen(true);
@@ -35,57 +35,83 @@ const BlockchainTips = () => {
 
 	const handleClose = () => {
 		setOpen(false);
-		setAmount("");
+		setBalance(null);
 	};
 
-	const handleTipClick = async () => {
-		const web3 = new Web3(Web3.givenProvider);
-		const accounts = await web3.eth.requestAccounts();
-		const account = accounts[0];
-		const balance = await web3.eth.getBalance(account);
-		const tipAmount = web3.utils.toWei(amount, "ether");
+	const handleConnect = async () => {
+		try {
+			const accounts = await window.ethereum.request({
+				method: "eth_requestAccounts",
+			});
+			setAccount(accounts[0]);
 
-		if (balance === "0") {
-			alert(
-				"Your account balance is 0 ETH. Please add funds to your account before sending a tip."
-			);
-			handleClose();
-			return;
+			const result = await ethereum.request({
+				method: "eth_getBalance",
+				params: [accounts[0], "latest"],
+			});
+			const wei = parseInt(result, 16);
+			const balance = wei / 10 ** 18;
+			console.log(balance + " ETH");
+		} catch (error) {
+			console.error("Failed to connect:", error);
 		}
-
-		await web3.eth.sendTransaction({
-			to: "0x...[recipient address]...",
-			from: account,
-			value: tipAmount,
-		});
-		handleClose();
 	};
 
-	const handleAmountChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-		setAmount(event.target.value);
+	const handleSend = async () => {
+		try {
+			const transactionParam = {
+				to: "0x45B6b39e1Cf8A6b4Ff2720f6BA0089d4574126E5",
+				from: account,
+				value: "0x38D7EA4C68000",
+			};
+
+			const txhash = await ethereum.request({
+				method: "eth_sendTransaction",
+				params: [transactionParam],
+			});
+			const confirmation = await checkTransactionConfirmation(txhash);
+			alert(confirmation);
+		} catch (error) {
+			console.error("Failed to send:", error);
+		}
+	};
+
+	const checkTransactionConfirmation = (txhash: string): Promise<string> => {
+		const checkTransactionLoop = (): Promise<string> => {
+			return ethereum
+				.request({ method: "eth_getTransactionReceipt", params: [txhash] })
+				.then(receipt => {
+					if (receipt != null) return "confirmed";
+					else return checkTransactionLoop();
+				});
+		};
+
+		return checkTransactionLoop();
 	};
 
 	return (
 		<Box>
 			<button onClick={handleOpen}>TIP</button>
+
 			<Dialog open={open} onClose={handleClose}>
 				<DialogTitle>Blockchain Tipping</DialogTitle>
-				<DialogContent>
-					<TextField
-						autoFocus
-						margin='dense'
-						id='tip-amount'
-						label='Amount'
-						type='number'
-						value={amount}
-						onChange={handleAmountChange}
-						fullWidth
-					/>
+				<DialogContent className='flex justify-around'>
+					<div>
+						{web3.currentProvider && (
+							<>
+								<button id='connect-button' onClick={handleConnect}>
+									Connect Metamask
+								</button>
+								{account && (
+									<button id='send-button' onClick={handleSend}>
+										Tip 0.001 ETH
+									</button>
+								)}
+							</>
+						)}
+						{!web3.currentProvider && <div>Please install MetaMask</div>}
+					</div>
 				</DialogContent>
-				<DialogActions>
-					<Button onClick={handleClose}>Cancel</Button>
-					<Button onClick={handleTipClick}>Tip</Button>
-				</DialogActions>
 			</Dialog>
 		</Box>
 	);
