2734b4473b4bde0725abe07ddebb34d21d91433c
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-04-12 11:59:06 +0200

    ifelse

--- server/routes/Analitycs/ReviewsFromUsers.js
+++ server/routes/Analitycs/ReviewsFromUsers.js
@@ -3,18 +3,23 @@ const sql = require("mssql");
 async function userComment(req, res, dbConfig) {
 	const { email, comment, category } = req.query;
 
-	try {
-		const pool = await sql.connect(dbConfig);
+	if (!email || !comment || !category) {
+		res.status(400).send("Missing parameters");
+		return;
+	} else {
+		try {
+			const pool = await sql.connect(dbConfig);
 
-		let result = await pool
-			.request()
-			.query(
-				`INSERT INTO Comment (email, comment, category) VALUES ('${email}', '${comment}', '${category}')`
-			);
+			let result = await pool
+				.request()
+				.query(
+					`INSERT INTO Comment (email, comment, category) VALUES ('${email}', '${comment}', '${category}')`
+				);
 
-		res.status(200).json({ userComment: result.recordset });
-	} catch (error) {
-		res.status(500).send(`${error}`);
+			res.status(200).json({ userComment: result.recordset });
+		} catch (error) {
+			res.status(500).send(`${error}`);
+		}
 	}
 }
 
