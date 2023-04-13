a5b39d192f47eeb5507795b75796fe391f73c1c9
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-04-13 19:59:40 +0200

    checks

--- server/routes/Top/Top-M.js
+++ server/routes/Top/Top-M.js
@@ -6,65 +6,73 @@ async function topMeasurements(req, res, dbConfig) {
 		const pool = await sql.connect(dbConfig);
 
 		let result;
-		if (gender === "male") {
-			if (unit === "cm") {
-				result = await pool
-					.request()
-					.input("chest", chest, "hips", hips, "waist", waist)
-					.query(
-						`SELECT Brand.Brand, Size, ChestCM_min,ChestCM_max, WaistCM_min WaistCM_max, HipCM_min, HipCM_max FROM TopsMan INNER JOIN Brand ON Brand.Id = TopsMan.BrandId WHERE ChestCM_min <= @chest AND ChestCM_max >= @chest AND HipCM_min <= @hips AND HipCM_max >= @hips`,
-						[
-							{ name: "chest", value: chest },
-							{ name: "hips", value: hips },
-							{ name: "waist", value: waist },
-						]
-					);
-
-				res.status(200).json({ topMeasurements: result.recordset });
-			} else {
-				result = await pool
-					.request()
-					.input("chest", chest, "hips", hips, "waist", waist)
-					.query(
-						`SELECT Brand.Brand, Size, ChestIN_min,ChestIN_max, WaistIN_min WaistIN_max, HipIN_min, HipIN_max FROM TopsMan INNER JOIN Brand ON Brand.Id = TopsMan.BrandId WHERE ChestIN_min <= @chest AND ChestIN_max >= @chest AND HipIN_min <= @hips AND HipIN_max >= @hips`,
-						[
-							{ name: "chest", value: chest },
-							{ name: "hips", value: hips },
-							{ name: "waist", value: waist },
-						]
-					);
-
-				res.status(200).json({ topMeasurements: result.recordset });
-			}
+		if (typeof chest === "undefined" || chest === null || chest === "") {
+			res.status(400).json({ error: "Chest measurement is required." });
+		} else if (typeof hips === "undefined" || hips === null || hips === "") {
+			res.status(400).json({ error: "Hip measurement is required." });
+		} else if (typeof waist === "undefined" || waist === null || waist === "") {
+			res.status(400).json({ error: "Waist measurement is required." });
 		} else {
-			if (unit === "cm") {
-				result = await pool
-					.request()
-					.input("chest", chest, "hips", hips, "waist", waist)
-					.query(
-						`SELECT Brand.Brand, Size, ChestCM_min,ChestCM_max, WaistCM_min WaistCM_max, HipCM_min, HipCM_max FROM TopsWoman INNER JOIN Brand ON Brand.Id = TopsWoman.BrandId WHERE ChestCM_min <= @chest AND ChestCM_max >= @chest AND HipCM_min <= @hips AND HipCM_max >= @hips`,
-						[
-							{ name: "chest", value: chest },
-							{ name: "hips", value: hips },
-							{ name: "waist", value: waist },
-						]
-					);
+			if (gender === "male") {
+				if (unit === "cm") {
+					result = await pool
+						.request()
+						.input("chest", chest, "hips", hips, "waist", waist)
+						.query(
+							`SELECT Brand.Brand, Size, ChestCM_min,ChestCM_max, WaistCM_min WaistCM_max, HipCM_min, HipCM_max FROM TopsMan INNER JOIN Brand ON Brand.Id = TopsMan.BrandId WHERE ChestCM_min <= @chest AND ChestCM_max >= @chest AND HipCM_min <= @hips AND HipCM_max >= @hips`,
+							[
+								{ name: "chest", value: chest },
+								{ name: "hips", value: hips },
+								{ name: "waist", value: waist },
+							]
+						);
 
-				res.status(200).json({ topMeasurements: result.recordset });
+					res.status(200).json({ topMeasurements: result.recordset });
+				} else {
+					result = await pool
+						.request()
+						.input("chest", chest, "hips", hips, "waist", waist)
+						.query(
+							`SELECT Brand.Brand, Size, ChestIN_min,ChestIN_max, WaistIN_min WaistIN_max, HipIN_min, HipIN_max FROM TopsMan INNER JOIN Brand ON Brand.Id = TopsMan.BrandId WHERE ChestIN_min <= @chest AND ChestIN_max >= @chest AND HipIN_min <= @hips AND HipIN_max >= @hips`,
+							[
+								{ name: "chest", value: chest },
+								{ name: "hips", value: hips },
+								{ name: "waist", value: waist },
+							]
+						);
+
+					res.status(200).json({ topMeasurements: result.recordset });
+				}
 			} else {
-				result = await pool
-					.request()
-					.input("chest", chest, "hips", hips, "waist", waist)
-					.query(
-						`SELECT Brand.Brand, Size, ChestIN_min,ChestIN_max, WaistIN_min WaistIN_max, HipIN_min, HipIN_max FROM TopsWoman INNER JOIN Brand ON Brand.Id = TopsWoman.BrandId WHERE ChestIN_min <= @chest AND ChestIN_max >= @chest AND HipIN_min <= @hips AND HipIN_max >= @hips`,
-						[
-							{ name: "chest", value: chest },
-							{ name: "hips", value: hips },
-							{ name: "waist", value: waist },
-						]
-					);
+				if (unit === "cm") {
+					result = await pool
+						.request()
+						.input("chest", chest, "hips", hips, "waist", waist)
+						.query(
+							`SELECT Brand.Brand, Size, ChestCM_min,ChestCM_max, WaistCM_min WaistCM_max, HipCM_min, HipCM_max FROM TopsWoman INNER JOIN Brand ON Brand.Id = TopsWoman.BrandId WHERE ChestCM_min <= @chest AND ChestCM_max >= @chest AND HipCM_min <= @hips AND HipCM_max >= @hips`,
+							[
+								{ name: "chest", value: chest },
+								{ name: "hips", value: hips },
+								{ name: "waist", value: waist },
+							]
+						);
 
-				res.status(200).json({ topMeasurements: result.recordset });
+					res.status(200).json({ topMeasurements: result.recordset });
+				} else {
+					result = await pool
+						.request()
+						.input("chest", chest, "hips", hips, "waist", waist)
+						.query(
+							`SELECT Brand.Brand, Size, ChestIN_min,ChestIN_max, WaistIN_min WaistIN_max, HipIN_min, HipIN_max FROM TopsWoman INNER JOIN Brand ON Brand.Id = TopsWoman.BrandId WHERE ChestIN_min <= @chest AND ChestIN_max >= @chest AND HipIN_min <= @hips AND HipIN_max >= @hips`,
+							[
+								{ name: "chest", value: chest },
+								{ name: "hips", value: hips },
+								{ name: "waist", value: waist },
+							]
+						);
+
+					res.status(200).json({ topMeasurements: result.recordset });
+				}
 			}
 		}
 	} catch (error) {
