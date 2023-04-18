43987d60f5338082ed99518f5f61441d6ca1cc15
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-04-18 10:00:49 +0200

    scroll top button

--- client/src/components/pages/ScrollTop.tsx
+++ client/src/components/pages/ScrollTop.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { TbArrowBigUpLines } from "react-icons/tb"
 
 const ScrollTop = () => {
+    const handleScroll = () => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+
+    };
+
     return (
-        <div>ScrollTop</div>
+        <button onClick={handleScroll} className='bg-primary rounded-full text-white p-2 fixed bottom-2 right-2'>
+            <TbArrowBigUpLines />
+        </button>
     )
 }
 
