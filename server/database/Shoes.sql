3fa047517175e8e3ee9bb48fc428d23276951069
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-05-16 19:38:01 +0200

    query fix

--- server/database/Shoes.sql
+++ server/database/Shoes.sql
@@ -1,1 +1,5 @@
-select * from FeedbackCount
+-- SELECT Brand.Brand, Size, ChestCM_min,ChestCM_max, WaistCM_min WaistCM_max, HipCM_min, HipCM_max,ChestIN_min,ChestIN_max, WaistIN_min WaistIN_max, HipIN_min, HipIN_max FROM TopsMan INNER JOIN Brand ON Brand.Id = TopsMan.BrandId
+-- WHERE ChestCM_min <= 130 AND ChestCM_max >= 130 AND HipCM_min <= 126 AND HipCM_max >= 126 AND WaistCM_min <= 120 AND WaistCM_max >= 120
+SELECT
+    Brand.Brand, Size, ChestCM_min, ChestCM_max, WaistCM_min, WaistCM_max, HipCM_min, HipCM_max, ChestIN_min, ChestIN_max, WaistIN_min, WaistIN_max, HipIN_min, HipIN_max
+FROM TopsMan INNER JOIN Brand ON Brand.Id = TopsMan.BrandId
