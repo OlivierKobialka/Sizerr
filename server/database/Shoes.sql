ede9f0efc9d18b0f38362bc457c230e097e979fb
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-05-16 20:18:55 +0200

    fix

--- server/database/Shoes.sql
+++ server/database/Shoes.sql
@@ -1,5 +1,24 @@
 -- SELECT Brand.Brand, Size, ChestCM_min,ChestCM_max, WaistCM_min WaistCM_max, HipCM_min, HipCM_max,ChestIN_min,ChestIN_max, WaistIN_min WaistIN_max, HipIN_min, HipIN_max FROM TopsMan INNER JOIN Brand ON Brand.Id = TopsMan.BrandId
 -- WHERE ChestCM_min <= 130 AND ChestCM_max >= 130 AND HipCM_min <= 126 AND HipCM_max >= 126 AND WaistCM_min <= 120 AND WaistCM_max >= 120
-SELECT
-    Brand.Brand, Size, ChestCM_min, ChestCM_max, WaistCM_min, WaistCM_max, HipCM_min, HipCM_max, ChestIN_min, ChestIN_max, WaistIN_min, WaistIN_max, HipIN_min, HipIN_max
-FROM TopsMan INNER JOIN Brand ON Brand.Id = TopsMan.BrandId
+-- SELECT
+--     Brand.Brand, Size, ChestCM_min, ChestCM_max, WaistCM_min, WaistCM_max, HipCM_min, HipCM_max, ChestIN_min, ChestIN_max, WaistIN_min, WaistIN_max, HipIN_min, HipIN_max
+-- FROM TopsMan INNER JOIN Brand ON Brand.Id = TopsMan.BrandId
+
+-- select * from ShoesMan
+
+create table ShoeSizeCounterEU (
+    35 int,
+    36 int,
+    37 int,
+    38 int,
+    39 int,
+    40 int,
+    41 int,
+    42 int,
+    43 int,
+    44 int,
+    45 int,
+    46 int,
+    47 int,
+    48 int,
+)
