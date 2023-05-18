e609c4ce53800884c6519f0974e087932727465c
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-05-18 11:03:44 +0200

    fix

--- client/src/components/pages/ScrollTop.tsx
+++ client/src/components/pages/ScrollTop.tsx
@@ -1,18 +1,16 @@
-import React from "react";
 import { TbArrowBigUpLines } from "react-icons/tb";
 
-const ScrollTop = () => {
-	const handleScroll = () => {
-		window.scrollTo({ top: 0, behavior: "smooth" });
-	};
+export default function ScrollTop() {
+    const handleScroll = () => {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    };
 
-	return (
-		<button
-			onClick={handleScroll}
-			className='bg-primary rounded-full text-white p-2 fixed bottom-3 right-3'>
-			<TbArrowBigUpLines />
-		</button>
-	);
-};
-
-export default ScrollTop;
+    return (
+        <button
+            onClick={handleScroll}
+            className="bg-primary rounded-full text-white p-2 fixed bottom-3 right-3"
+        >
+            <TbArrowBigUpLines />
+        </button>
+    );
+}
